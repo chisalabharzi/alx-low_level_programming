3fed31df5d0e723f65a9dcedac0d3f273cf19df9
chisalabharzi <chisalabharzi17@gmail.com>
2023-02-21 17:46:36 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
  * main - fibonacci <3
- *
  * Purpose - no hardcode
  * Return:  (Success)
  */
@@ -10,7 +9,7 @@ int main(void)
 unsigned long int i;
 unsigned long int bef = 1;
 unsigned long int aft = 2;
-unsigned long int l = 1000000000;	
+unsigned long int l = 1000000000;
 unsigned long int bef1;
 unsigned long int bef2;
 unsigned long int aft1;
