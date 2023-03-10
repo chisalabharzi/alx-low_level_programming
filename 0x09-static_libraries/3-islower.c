a3e0af89c5506f8e91894b92cbfbbe7aa9566ef0
chisalabharzi <chisalabharzi17@gmail.com>
2023-03-10 14:59:49 -0800

    libmy.c

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,19 +1,10 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-/**
- * main - check the code.
- * Return: Always 0.
- */
-int main(void)
+int _islower(int c)
 {
-	int r;
-
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+    return (c >= 'a' && c <= 'z');
 }
