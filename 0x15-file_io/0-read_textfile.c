ca6c083757a8d5d013dac52259bc27d188ca55a8
chisalabharzi <chisalabharzi17@gmail.com>
2023-04-08 00:38:59 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include "main.h"
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	if (filename == NULL || *filename == '\0')
+		return (0);
+
+	int fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+	char *buffer = malloc(sizeof(char) * letters);
+
+	if (buffer == NULL)
+		return (0);
+
+	ssize_t bytes_read = read(fd, buffer, letters);
+
+	if
+		(bytes_read == -1)
+		{
+			free(buffer);
+			return (0);
+		}
+
+	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
+	if
+		(bytes_written == -1 || bytes_written != bytes_read)
+		{
+			free(buffer);
+			return (0);
+		}
+
+	free(buffer);
+	close(fd);
+
+	return (bytes_read);
+}
+
