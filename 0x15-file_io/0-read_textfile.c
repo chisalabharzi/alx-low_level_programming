2a4c2d9f3dd3bd0edaebe449eede372946e189ee
chisalabharzi <chisalabharzi17@gmail.com>
2023-05-12 14:12:00 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - reads a teaxt file and prints it to the POSIX standard output
+ * @filename: name of file to be read
+ * @letters: numbers and letters that should be read and printed
+ * Return: actual numbers and letters that it shpuld be read and printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters);
+{
+	int fd;
+	ssize_t lenr, lenw;
+	char *buffer;
+
+	if (filename==NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) *letters);
+	if (buffer == NULL)
+	{
+		clos(fd);
+		return (0);
+	}
+	lenr = read(fd, buffer, letters);
+	close(fd);
+	if (lenr == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	lenw = write(STDOUT_FILENO, buffer, lenr);
+	free(buffer);
+	if (lenr != lenw)
+		return (0);
+	return (lenw);
+}
